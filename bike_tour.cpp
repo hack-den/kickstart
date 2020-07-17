b2d732a94873ef5e2f261cffbfa0587930fb1da4
Vaibhav Madan <66436987+hack-den@users.noreply.github.com>
2020-07-17 18:09:27 +0530

    Add files via upload
    Google Kickstart 2020 - Bike Tour (in C++)

---
+++ bike_tour.cpp
@@ -0,0 +1,28 @@
+# include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int t,l;
+  cin >> t;
+  for (l = 1; l <= t; l++) {
+    int n,y = 0;
+    cin >> n;
+    int c[n];
+
+    // input the integers
+    for (int i = 0; i < n; i++) {
+      cin >> c[i];
+    }
+
+    for (int l = 1; l < n; l++) {
+      if (c[l] > c[l-1] and c[l] > c[l+1]) {
+        y += 1;
+      }
+    }
+
+    // print the answer in Google Kickstart format
+    cout << "Case #" << l << ": " << y << endl;
+  }
+
+}
